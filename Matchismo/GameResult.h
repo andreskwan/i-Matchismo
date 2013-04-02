cd05302ada77bce10f079ffb9b6d86fe6b370873
ANDRES KWAN <andreskwan@gmail.com>
2013-04-01 19:36:39 -0500

    B-V5 - adding class GameResult - must be part of the model

---
+++ Matchismo/GameResult.h
@@ -0,0 +1,13 @@
+//
+//  GameResult.h
+//  Matchismo
+//
+//  Created by Andres Kwan on 4/1/13.
+//  Copyright (c) 2013 Andres Kwan O. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameResult : NSObject
+
+@end
---
+++ Matchismo/GameResult.m
@@ -0,0 +1,13 @@
+//
+//  GameResult.m
+//  Matchismo
+//
+//  Created by Andres Kwan on 4/1/13.
+//  Copyright (c) 2013 Andres Kwan O. All rights reserved.
+//
+
+#import "GameResult.h"
+
+@implementation GameResult
+
+@end
