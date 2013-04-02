1d2aee65553812b57fd49337a61b07261820dc71
ANDRES KWAN <andreskwan@gmail.com>
2013-04-01 19:12:03 -0500

    B-V5 - adding GameResultViewCOntroler class

---
+++ Matchismo/GameResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameResultViewController.h
+//  Matchismo
+//
+//  Created by Andres Kwan on 4/1/13.
+//  Copyright (c) 2013 Andres Kwan O. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameResultViewController : UIViewController
+
+@end
---
+++ Matchismo/GameResultViewController.m
@@ -0,0 +1,35 @@
+//
+//  GameResultViewController.m
+//  Matchismo
+//
+//  Created by Andres Kwan on 4/1/13.
+//  Copyright (c) 2013 Andres Kwan O. All rights reserved.
+//
+
+#import "GameResultViewController.h"
+
+@interface GameResultViewController ()
+
+@end
+
+@implementation GameResultViewController
+
+-(void)setup
+{
+    
+}
+
+-(void)awakeFromNib
+{
+    
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    [self setup];
+    return self;
+}
+
+
+@end
